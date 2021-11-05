75d9034e96b0030d0c99cff19808bae145f0d058
dimrogovoy <rogovoi.dmitrii.a@gmail.com>
2021-11-05 13:29:55 +0200

    Edit task Find the odd int

--- Find_the_odd_int/main.go
+++ Find_the_odd_int/main.go
@@ -7,27 +7,23 @@ import (
 
 func FindOdd(seq []int) int {
 	sort.Ints(seq)
-	var result int
 	count := 1
 	for i := 1; i < len(seq); i++ {
 		if seq[i-1] == seq[i] {
 			count++
 		}
 		if seq[i-1] != seq[i] {
-			result = count % 2
-			if result != 0 {
+			if count % 2 != 0 {
 				return seq[i-1]
 			}
 			count = 1
 		}
-
 	}
 	return seq[len(seq)-1]
 }
 
 func main() {
-	seq := []int{
-		5, 4, 3, 2, 1, 5, 4, 3, 2, 10, 10}
+	seq := []int{5, 4, 3, 2, 1, 5, 4, 3, 2, 10, 10}
 
 	fmt.Println(FindOdd(seq))
 }
